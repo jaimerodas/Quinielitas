4bc0ad6b6b0a262d5423a8642c17ff5c20b1ea73
Jaime Rodas <jaime@rodas.mx>
2018-05-27 16:40:59 -0500

    Init dashboards

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Handles logins/logouts
+class DashboardsController < ApplicationController
+  before_action :signed_in?
+
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'dashboards#show'
   resources :matches, except: %i[show]
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'sign_out', to: 'sessions#destroy'
