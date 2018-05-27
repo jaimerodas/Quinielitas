3e543ae988f2f6985e827f0bdc3bd2f425c18add
Jaime Rodas <jaime@rodas.mx>
2018-05-27 10:47:37 -0500

    Configures rubocop

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Define an application-wide content security policy
 # For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 # Rails.application.config.content_security_policy do |policy|
 #   policy.default_src :self, :https
